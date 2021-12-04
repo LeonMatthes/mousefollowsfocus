c724c6c16468e2c9ae6ad5fa36b6d31428c1c524
Leon Matthes <leon@matthes.biz>
2021-12-04 23:41:02 +0100

    Jump to search results after closing overview
    Fix bug with focus-on-hover that caused eratic pointer jumps

--- extension.js
+++ extension.js
@@ -18,11 +18,16 @@ function get_window_actor(window) {
     return undefined;
 }
 
-function cursor_within_rect(mouse_x, mouse_y, rect) {
-    return mouse_x >= rect.x &&
-        mouse_x <= rect.x + rect.width &&
-        mouse_y >= rect.y &&
-        mouse_y <= rect.y + rect.height;
+function cursor_within_window(mouse_x, mouse_y, win) {
+    // use get_buffer_rect instead of get_frame_rect here, because the frame_rect may
+    // exclude shadows, which might already cause a focus-on-hover event, therefore causing
+    // the pointer to jump around eratically.
+    let rect = win.get_buffer_rect();
+
+    return mouse_x >= rect.x - margins &&
+        mouse_x <= rect.x + rect.width  + margins &&
+        mouse_y >= rect.y - margins &&
+        mouse_y <= rect.y + rect.height + margins;
 }
 
 // logging disabled by default
@@ -38,12 +43,12 @@ function focus_changed(win) {
     const actor = get_window_actor(win);
     dbg_log('window focus event received');
     if (actor) {
-        let rect = win.get_frame_rect();
+        let rect = win.get_buffer_rect();
 
         let seat = Clutter.get_default_backend().get_default_seat();
         let [mouse_x, mouse_y, _] = global.get_pointer();
 
-        if (cursor_within_rect(mouse_x, mouse_y, rect)) {
+        if (cursor_within_window(mouse_x, mouse_y, win)) {
             dbg_log('pointer within window, discarding event');
         } else if (overview.visible) {
             dbg_log('overview visible, discarding event');
@@ -63,6 +68,21 @@ function connect_to_window(win) {
     }
 
     win._mousefollowsfocus_extension_signal = win.connect('focus', focus_changed);
+}
+
+function get_focused_window() {
+    for (const actor of global.get_window_actors()) {
+        if (actor.is_destroyed()) {
+            continue;
+        }
+
+        const win = actor.get_meta_window();
+        if (win.has_focus()) {
+            return win;
+        }
+    }
+
+    return undefined;
 }
 
 class Extension {
@@ -86,6 +106,15 @@ class Extension {
 
             connect_to_window(win);
         });
+
+        this.hide_signal = overview.connect('hidden', function() {
+            // the focus might change whilst we're in the overview, i.e. by
+            // searching for an already open app.
+            const win = get_focused_window();
+            if (win !== undefined) {
+                focus_changed(win)
+            }
+        });
     }
 
     // REMINDER: It's required for extensions to clean up after themselves when
@@ -96,6 +125,11 @@ class Extension {
         if (this.create_signal !== undefined) {
             global.display.disconnect(this.create_signal);
             this.create_signal = undefined;
+        }
+
+        if (this.hide_signal !== undefined) {
+            overview.disconnect(this.hide_signal);
+            this.hide_signal = undefined;
         }
 
         for (const actor of global.get_window_actors()) {
