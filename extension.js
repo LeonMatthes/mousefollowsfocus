a769af88169b9913be641faa1a334f014d190e0a
thjderjktyrjkt <40300931+thjderjktyrjkt@users.noreply.github.com>
2022-11-18 08:41:09 +0000

    optimize within window check

--- extension.js
+++ extension.js
@@ -26,14 +26,17 @@ function cursor_within_window(mouse_x, mouse_y, win) {
     // > the pointer to jump around eratically.
     // `get_frame_rect` is used again, because now the extension doesn't rely on arbitrary
     // focus change event. So making the rect more precise helps with reducing mouse travel.
-    let rect = win.get_frame_rect();
+    const cursor_rect = new Meta.Rectangle({ x: mouse_x, y: mouse_y, width: 1, height: 1 });
+    return cursor_rect.intersect(win.get_frame_rect())[0];
+
+/*     let rect = win.get_frame_rect();
 
     dbg_log(`window rect: ${rect.x}:${rect.y} - ${rect.width}:${rect.height}`);
 
     return mouse_x >= rect.x &&
         mouse_x <= rect.x + rect.width &&
         mouse_y >= rect.y &&
-        mouse_y <= rect.y + rect.height;
+        mouse_y <= rect.y + rect.height; */
 }
 
 // logging disabled by default
